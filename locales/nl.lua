fe5acab41e8c80a9fc829e836af11bb25b202b2a
bedar89 <bedar89@gmail.com>
2022-07-28 15:33:50 +0000

    translate --> nl

---
+++ locales/nl.lua
@@ -0,0 +1,26 @@
+Locales['en'] = {
+	["ticket_expired"]  = "Je ticket is verlopen.",
+	["time_left_count"] = "Tijd over %.2d:%.2d",
+
+
+	["player_left_arcade"] = "Je hebt het game hal verlaten, je ticket wordt niet meer geaccepteerd.",
+	["need_to_buy_ticket"] = "Ik moet eerst een kaartje kopen!",
+	["open_ticket_menu"]   = "druk op ~INPUT_CONTEXT~ om met de coole kerel te praten.",
+	["open_computer"]      = "druk op ~INPUT_CONTEXT~ om de computer te openen",
+	["not_enough_money"]   = "Helaas heb je niet genoeg geld.",
+	["bought_ticket"]      = "Je hebt een kaartje gekocht %s, geniet van je tijd in de arcade. Vergeet niet dat je: %s minuten over hebt",
+
+	["bronz"]  = "bronz",
+	["silver"] = "zilver",
+	["gold"]   = "goud",
+
+	["ticket_label"] = "ticket %s",
+
+	["ticket_menu"]     = "Koop een ticket",
+	["computer_menu"]   = "Computer menu",
+
+	["give_back_ticket"]   = "Wil je je ticket retourneren ?",
+
+	["yes"] = "ja",
+	["no"] = "nee",
+}
